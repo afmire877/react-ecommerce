48049371d08c0de0dec90af32b13a89bb951352b
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-10-27 23:29:13 +0000

    Added testing firebase Auth

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {LogIn} from '../auth/firebase'
+
+class App extends React.Component { 
+
+
+
+    render() {
+        return (
+            <LogIn />
+        )
+    }
+}
+
+
+export default App;
